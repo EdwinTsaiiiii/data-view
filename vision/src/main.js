fa5a500a24c0bd4ada8f0fa021e7dcd8469242c2
1245667035@qq.com,EdwinTsaiiiii <1245667035@qq.com>
2023-01-22 17:37:22 +0800

    完成销量版块的可视化

--- vision/src/main.js
+++ vision/src/main.js
@@ -3,9 +3,10 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import axios from "axios";
-
+import './assets/css/global.less';
+import './assets/font/iconfont.css'
 // 配置请求基准路径
-axios.defaults.baseURL = "http://127.0.0.1:8889";
+axios.defaults.baseURL = "http://127.0.0.1:8888/api/";
 // 将axios挂载到Vue的原型对象上
 Vue.prototype.$http = axios;
 
---
+++ vision/src/view/SellerPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="com-page">
+		<Seller></Seller>
+	</div>
+</template>
+<script>
+import Seller from "@/components/Seller.vue";
+export default {
+	components:{
+		Seller
+	},
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+<style lang="less" scoped></style>
